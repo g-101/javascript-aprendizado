8244d28c3a851d044b2f713a22bd77f9483b3858
g-101 <gyreplicant@gmail.com>
2022-03-07 10:21:33 -0300

    escopo

---
+++ blockScope.js
@@ -0,0 +1,11 @@
+let radius = 8;
+if(radius > 0) {
+    const PI = 3.1415;
+    let msg = 'hiii';
+}
+
+// console.log(PI);
+// console.log(msg);
+// const PI e let msg foram declaradas dentro do block if
+// por isso não podem ser acessadas
+console.log(radius);
