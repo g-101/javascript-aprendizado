8e1f6f70b35e75a8f0dc8c3041a078599aeafdec
g-101 <gyreplicant@gmail.com>
2022-03-07 08:28:34 -0300

    primeiro commit

---
+++ variaveis.js
@@ -0,0 +1,45 @@
+//Declarando uma variavel
+var xoxo;
+
+// let e var ao ser declaradas tem o valor padrão de undefined
+let a;
+var b;
+console.log(a, b);
+
+//O comum é declarar e iniciar com algum valor padrão
+let xoxo = 8;
+
+
+// const DEVE ser declarada e inicializada
+
+// const c;   // SyntaxError: Missing initializer in const declaration
+const c = 3.1415;
+console.log(c);         
+
+//Diferenças entre VAR, LET e CONST
+
+// VAR
+// Pode ser redeclarada
+// Pode ser reasign
+
+// Tem Escopo:
+// Global
+// De função
+
+
+// LET
+// Não pode ser redeclarada
+// Pode ter o valor atualizado
+
+// Tem Escopo:
+// De função
+// De bloco
+
+// CONST
+
+// Não pode ser redeclarada
+// Não pode ter o valor atualizado
+
+// Tem Escopo:
+// De função
+// De bloco
