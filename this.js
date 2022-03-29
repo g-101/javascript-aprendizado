0ca1e398dd366b3b6269272803a7988910962411
g-101 <gyreplicant@gmail.com>
2022-03-29 19:50:19 -0300

    comments about this keyword

--- this.js
+++ this.js
@@ -1,13 +1,15 @@
+// o this é uma keyword especial que funciona de acordo com o contexto de execução
+// o this pega o valor e aponta para o "dono" da função em que o this está sendo executado
+
+// O this no contexto de método:
 const cat = {
     name: "Maru",
     color: "yellow",
     meow(){
-        console.log(`object: ${this}`);
         console.log(`${this.name} says meowww`);
+        // aponta para o "dono" que é o objeto cat, no atributo/propriedade name.
     }
 }
-
 cat.meow();
 
-const meow2 = cat.meow;
-meow2();
+
