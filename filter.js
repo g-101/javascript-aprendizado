fdc6c3788eddcb06bbea0e65374bd5c426834eb4
g101 <gyreplicant@gmail.com>
2022-05-09 15:52:45 -0300

    organizando diretorios

--- filter.js
+++
@@ -1,8 +0,0 @@
-// cria um novo array com todos os elementos que passaram no teste
-const numbers = [0, 3, 4, 9, 99];
-//vai retornar todos os numeros menor que 5
-const smallNumbers = numbers.filter(n => n < 5);
-console.log(smallNumbers);
-
-const validNumbers = numbers => numbers.filter(n => n < 5);
-console.log(validNumbers(numbers));
