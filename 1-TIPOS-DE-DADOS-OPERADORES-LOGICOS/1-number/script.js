8d3a1ec635d43b865023bb236be8c0480999c341
g101 <gyreplicant@gmail.com>
2022-05-09 16:00:11 -0300

    tipo number

--- 1-TIPOS-DE-DADOS-OPERADORES-LOGICOS/1-number/script.js
+++ 1-TIPOS-DE-DADOS-OPERADORES-LOGICOS/1-number/script.js
@@ -1,3 +1,5 @@
-console.log(5)
-console.log(12.7)
-console.log(-123)
+// tipo Number: São os inteiros e floats
+// tipo float: são numeros com decimais
+console.log(typeof 5)
+console.log( typeof 12.7)
+console.log( typeof -123)
