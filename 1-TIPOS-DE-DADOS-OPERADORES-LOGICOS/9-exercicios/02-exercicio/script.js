03c7cccd916a4be7be313bf1cac3654e92f98f54
g101 <gyreplicant@gmail.com>
2022-05-10 10:06:46 -0300

    exercicios

---
+++ 1-TIPOS-DE-DADOS-OPERADORES-LOGICOS/9-exercicios/02-exercicio/script.js
@@ -0,0 +1,9 @@
+// Escreva três valores em number e exiba no navegador com o console.log()
+// Com numeros inteiros, decimais e por aritmetica
+
+
+console.log(42)
+console.log(41.9)
+console.log(40 + 2)
+
+
---
+++ 1-TIPOS-DE-DADOS-OPERADORES-LOGICOS/9-exercicios/05-exercicio/script.js
@@ -0,0 +1,10 @@
+// Faça uma operação que emita NaN
+
+// qualquer operação matematica com NaN, resulta em NaN
+
+
+console.log( 1 + NaN)
+// qualquer operação (exceto soma), resulta NaN
+console.log( "hello" * 2)
+
+
