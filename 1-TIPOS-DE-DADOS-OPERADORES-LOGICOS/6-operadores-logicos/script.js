ce91fe199b3abb41dae479325e20c980c5327f99
g101 <gyreplicant@gmail.com>
2022-05-11 10:59:39 -0300

    valores e operadores logicos e valores vazios

---
+++ 1-TIPOS-DE-DADOS-OPERADORES-LOGICOS/6-operadores-logicos/script.js
@@ -0,0 +1,17 @@
+// OPERADORES LOGICOS POR MEIO DE COMPARAÇÃO RESULTAM EM true ou false
+const v1 = 5 > 3 && 3 === 2
+const v2 = !true && true// COMPARAÇÕES GERAM BOOLEANS
+const v3 = "Angel" == "Angel" || 3 === "3"
+const v4 = 1 === 1
+
+
+console.log(v1)
+console.log(v2)
+console.log(v3)
+console.log(v4)
+
+// Operador ternario
+// Se a comparação for true, retorna o primeiro valor
+// Senão, retornar o segundo valor
+console.log(5 > 2 ? "Yes" : "No")
+
