55deac087d9b96a1d5d70747da376f17c0d50dd6
g101 <gyreplicant@gmail.com>
2022-05-11 11:00:15 -0300

    exercicios

---
+++ 1-TIPOS-DE-DADOS-OPERADORES-LOGICOS/8-exercicios/01-exercicio/script.js
@@ -0,0 +1,9 @@
+// Escreva três valores em string e exiba no navegador com o console.log()
+// Com aspas duplas, simples e templates literals
+const message = "h4x0r"
+
+console.log("Hello World")
+console.log('"Fsociety"')
+console.log(`${message}`)
+
+
---
+++ 1-TIPOS-DE-DADOS-OPERADORES-LOGICOS/8-exercicios/04-exercicio/script.js
@@ -0,0 +1,9 @@
+// Escreva três comparações com operadores logicos
+// Com and, or, not
+
+
+console.log( !(1 === 1))
+console.log( 5 > 3 && 3 >= 2)
+console.log(40 != 2 || 5 < 5)
+
+
