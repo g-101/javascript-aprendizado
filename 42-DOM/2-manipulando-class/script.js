ed239c65da84e3e34e3255f412992e28110acd63
g101 <gyreplicant@gmail.com>
2022-05-17 15:55:34 -0300

    refatorando

--- 42-DOM/2-manipulando-class/script.js
+++ 42-DOM/2-manipulando-class/script.js
@@ -1,6 +1,21 @@
 function clear() {
     document.querySelector("#exemplo").classList.value = ""
 }
+function changeStatusPhone() {
+    const phone = document.querySelector("#phone")
+    if(phone.style.display === "none" || phone.style.display === "") {
+        phone.style.display = "block"
+        document.querySelector("#btPhone").setAttribute("value", "Esconder Telefone")
+    }
+    else {
+        phone.style.display = "none" 
+        document.querySelector("#btPhone").setAttribute("value", "Mostrar Telefone")
+    }
+    
+}
+document.querySelector("#btPhone").addEventListener("click", () => changeStatusPhone() )
+
+
 
 document.querySelector(".btGreen").addEventListener("click", () => {
     clear()
@@ -16,3 +31,4 @@ document.querySelector(".btBlue").addEventListener("click", () => {
     clear()
     document.querySelector("#exemplo").classList.add("btBlue")
 })
+
