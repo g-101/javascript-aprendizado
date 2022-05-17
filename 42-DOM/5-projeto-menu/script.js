bdf8bba377837513ab0c6c9cda7d491fc292fc2f
g101 <gyreplicant@gmail.com>
2022-05-17 15:54:18 -0300

    projeto menu

---
+++ 42-DOM/5-projeto-menu/script.js
@@ -0,0 +1,18 @@
+function changeMenu() {
+    const menuArea = document.getElementById("menu-area");
+    if(menuArea.classList.contains("menu-opened")) {
+        
+        menuArea.classList.remove("menu-opened"); 
+        menuArea.classList.add("menu-closed"); 
+        
+    }
+    else {
+        menuArea.classList.remove("menu-closed"); 
+        menuArea.classList.add("menu-opened"); 
+    }
+}
+
+function main() {
+    document.getElementById("menu-opener").addEventListener("click", () => changeMenu());
+}
+main()
